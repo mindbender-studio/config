c3207001a89b20a2c4b6ca21e97193787c66218a
Marcus Ottosson <konstruktion@gmail.com>
2017-06-16 11:58:49 +0100

    Implement uninstall

--- polly/__init__.py
+++ polly/__init__.py
@@ -1,12 +1,15 @@
 import os
+from pyblish import api as pyblish
 
 PACKAGE_DIR = os.path.dirname(__file__)
 PLUGINS_DIR = os.path.join(PACKAGE_DIR, "plugins")
+PUBLISH_PATH = os.path.join(PLUGINS_DIR, "publish")
 
 
 def install():
-    from pyblish import api as pyblish
-    publish_path = os.path.join(PLUGINS_DIR, "publish")
-
     print("Registering global plug-ins..")
-    pyblish.register_plugin_path(publish_path)
+    pyblish.register_plugin_path(PUBLISH_PATH)
+
+
+def uninstall():
+    pyblish.deregister_plugin_path(PUBLISH_PATH)
--- polly/maya/__init__.py
+++ polly/maya/__init__.py
@@ -1,22 +1,33 @@
 import os
 
+from mindbender import api as mindbender
+from pyblish import api as pyblish
+
 from . import menu
 
 PARENT_DIR = os.path.dirname(__file__)
 PACKAGE_DIR = os.path.dirname(PARENT_DIR)
 PLUGINS_DIR = os.path.join(PACKAGE_DIR, "plugins")
 
+PUBLISH_PATH = os.path.join(PLUGINS_DIR, "maya", "publish")
+LOAD_PATH = os.path.join(PLUGINS_DIR, "maya", "load")
+CREATE_PATH = os.path.join(PLUGINS_DIR, "maya", "create")
+
 
 def install():
     from mindbender import api as mindbender
     from pyblish import api as pyblish
-    publish_path = os.path.join(PLUGINS_DIR, "maya", "publish")
-    load_path = os.path.join(PLUGINS_DIR, "maya", "load")
-    create_path = os.path.join(PLUGINS_DIR, "maya", "create")
 
-    print("Registering Maya plug-ins..")
-    pyblish.register_plugin_path(publish_path)
-    mindbender.register_plugin_path(mindbender.Loader, load_path)
-    mindbender.register_plugin_path(mindbender.Creator, create_path)
+    pyblish.register_plugin_path(PUBLISH_PATH)
+    mindbender.register_plugin_path(mindbender.Loader, LOAD_PATH)
+    mindbender.register_plugin_path(mindbender.Creator, CREATE_PATH)
 
     menu.install()
+
+
+def uninstall():
+    pyblish.deregister_plugin_path(PUBLISH_PATH)
+    mindbender.deregister_plugin_path(mindbender.Loader, LOAD_PATH)
+    mindbender.deregister_plugin_path(mindbender.Creator, CREATE_PATH)
+
+    menu.uninstall()
